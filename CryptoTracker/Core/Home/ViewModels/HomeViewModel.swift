045d1ae6b36ca3a1e969cbb5ffa297bbf5e8b504
nurdinaffandidev <nurdinaffandidev@gmail.com>
2025-03-27 22:32:26 +0800

    feat[HomeViewModel]: implement HomeViewModel

---
+++ CryptoTracker/Core/Home/ViewModels/HomeViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewModel.swift
+//  CryptoTracker
+//
+//  Created by nurdin affandi on 27/3/25.
+//
+
+import Foundation
+
+class HomeViewModel: ObservableObject {
+    @Published var allCoins: [Coin] = []
+    @Published var portfolioCoins: [Coin] = []
+    
+    init() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            self.allCoins.append(DeveloperPreview.shared.coin)
+            self.portfolioCoins.append(DeveloperPreview.shared.coin)
+        }
+    }
+}
