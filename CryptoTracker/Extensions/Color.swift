8a42e132206f77b095f18a441a7a6d99267d14a5
nurdinaffandidev <nurdinaffandidev@gmail.com>
2025-03-27 17:18:45 +0800

    feat: initial commit

---
+++ CryptoTracker/Extensions/Color.swift
@@ -0,0 +1,21 @@
+//
+//  Color.swift
+//  CryptoTracker
+//
+//  Created by nurdin affandi on 27/3/25.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+}
