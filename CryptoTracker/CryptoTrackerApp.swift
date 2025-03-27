ac7594004e8a4a772ba348a4900963b5f641059c
nurdinaffandidev <nurdinaffandidev@gmail.com>
2025-03-27 16:40:00 +0800

    Initial Commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by nurdin affandi on 27/3/25.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CryptoTrackerTests/CryptoTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerTests.swift
+//  CryptoTrackerTests
+//
+//  Created by nurdin affandi on 27/3/25.
+//
+
+import Testing
+@testable import CryptoTracker
+
+struct CryptoTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
