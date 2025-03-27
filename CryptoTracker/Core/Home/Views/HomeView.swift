501e71ebf4f8f09779a04f47a3c6d3102caafe23
nurdinaffandidev <nurdinaffandidev@gmail.com>
2025-03-27 18:45:13 +0800

    feat[HomeView]: implement HomeHeader in home view

---
+++ CryptoTracker/Core/Home/Views/HomeView.swift
@@ -0,0 +1,60 @@
+//
+//  HomeView.swift
+//  CryptoTracker
+//
+//  Created by nurdin affandi on 27/3/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    @State private var showPortfolio: Bool = false
+    
+    var body: some View {
+        ZStack {
+            // background layer
+            Color.theme.background
+                .ignoresSafeArea()
+            
+            // content layer
+            VStack {
+                HomeHeader
+                Spacer()
+            }
+        }
+    }
+}
+
+#Preview {
+    NavigationView {
+        HomeView()
+            .navigationBarHidden(true)
+    }
+}
+
+extension HomeView {
+    private var HomeHeader: some View {
+        HStack {
+            CircleButtonView(iconName: showPortfolio ? "plus" : "info")
+                .animation(.none, value: showPortfolio)
+                .background(
+                    CircleButtonAnimationView(animate: $showPortfolio)
+                )
+            Spacer()
+            Text(showPortfolio ? "Portfolio" : "Live Prices")
+                .font(.headline)
+                .fontWeight(.heavy)
+                .foregroundColor(Color.theme.accent)
+            Spacer()
+            CircleButtonView(iconName: "chevron.right")
+                .rotationEffect(.degrees(showPortfolio ? 180 : 0))
+                .onTapGesture {
+                    withAnimation {
+                        showPortfolio.toggle()
+                    }
+                }
+        }
+        .padding(.horizontal)
+    }
+}
