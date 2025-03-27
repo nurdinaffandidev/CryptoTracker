16652470758eddbfb89a2f3df52b086d9fa08747
nurdinaffandidev <nurdinaffandidev@gmail.com>
2025-03-27 21:33:07 +0800

    feat[CoinRowView]: implement CoinRowView

---
+++ CryptoTracker/Core/Home/Views/CoinRowView.swift
@@ -0,0 +1,69 @@
+//
+//  CoinRowView.swift
+//  CryptoTracker
+//
+//  Created by nurdin affandi on 27/3/25.
+//
+
+import SwiftUI
+
+struct CoinRowView: View {
+    let coin: Coin
+    let showHoldingsColumn: Bool
+    
+    var body: some View {
+        HStack {
+            leftColumn
+            Spacer()
+            if showHoldingsColumn {
+                centerColumn
+            }
+            rightColumn
+        }
+        .font(.subheadline)
+    }
+}
+
+#Preview(traits: .sizeThatFitsLayout){
+    CoinRowView(coin: DeveloperPreview.shared.coin, showHoldingsColumn: true)
+}
+
+extension CoinRowView {
+    private var leftColumn: some View {
+        HStack(spacing: 0) {
+            Text("\(coin.rank)")
+                .font(.caption)
+                .foregroundColor(Color.theme.secondaryText)
+                .frame(minWidth: 30)
+            Circle()
+                .frame(width: 30, height: 30)
+            Text(coin.symbol.uppercased())
+                .font(.headline)
+                .padding(.leading, 6)
+                .foregroundColor(Color.theme.accent)
+        }
+    }
+    
+    private var centerColumn: some View {
+        VStack(alignment: .trailing) {
+            Text(coin.currentHoldingsValue.asCurrencyWith6Decimals())
+                .bold()
+            Text(coin.currentHoldings?.asNumberString() ?? "")
+        }
+        .foregroundColor(Color.theme.accent)
+    }
+    
+    private var rightColumn: some View {
+        VStack(alignment: .trailing) {
+            Text(coin.currentPrice.asCurrencyWith2Decimals())
+                .bold()
+                .foregroundColor(Color.theme.accent)
+            Text(coin.priceChangePercentage24H?.asPercentString() ?? "")
+                .foregroundColor(
+                    (coin.priceChangePercentage24H ?? 0) > 0 ?
+                    Color.theme.green : Color.theme.red
+                )
+        }
+        .frame(width: UIScreen.main.bounds.width * 0.3)
+    }
+}
