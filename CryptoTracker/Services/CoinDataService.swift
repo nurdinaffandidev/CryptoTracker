486242ca23bb8d6d61912ab62d4900df97b2fe83
nurdinaffandidev <nurdinaffandidev@gmail.com>
2025-03-30 16:51:04 +0800

    feat[CoinDataService]: implement CoinDataService to get all coins

---
+++ CryptoTracker/Services/CoinDataService.swift
@@ -0,0 +1,46 @@
+//
+//  CoinDataService.swift
+//  CryptoTracker
+//
+//  Created by nurdin affandi on 30/3/25.
+//
+
+import Foundation
+import Combine
+
+class CoinDataService {
+    @Published var allCoins: [Coin] = []
+    var coinSubscriptions: AnyCancellable?
+    
+    init() {
+        getAllCoins()
+    }
+    
+    private func getAllCoins() {
+        let urlString = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h"
+        guard let url = URL(string: urlString) else { return }
+        
+        coinSubscriptions = URLSession.shared.dataTaskPublisher(for: url)
+            .subscribe(on: DispatchQueue.global(qos: .default))
+            .tryMap { output -> Data in
+                guard let response = output.response as? HTTPURLResponse,
+                      response.statusCode >= 200 && response.statusCode < 300 else {
+                          throw URLError(.badServerResponse)
+                      }
+                return output.data
+            }
+            .receive(on: DispatchQueue.main)
+            .decode(type: [Coin].self, decoder: JSONDecoder())
+            .sink { completion in
+                switch completion {
+                case .finished: break
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            } receiveValue: { [weak self] returnedCoins in
+                self?.allCoins = returnedCoins
+                self?.coinSubscriptions?.cancel()
+            }
+    }
+    
+}
